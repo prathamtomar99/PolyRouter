a554479e1667f79d6f9b9d8a6a3e3df1f632d8b2
prathamtomar99 <partham68209@gmail.com>
2026-05-17 11:39:29 +0530

    Added Abstract class for all Clients

---
+++ Exceptions.py
@@ -0,0 +1,1 @@
+# this file will be responsible for defination of all the user defined Exceptions
--- LLMClients.py
+++ LLMClients.py
@@ -1,1 +1,35 @@
 # File act as a interface between user defined universal call functions and llm clients specific invoke functions
+
+
+# ALL the clients to which i can make calls
+
+from abc import ABC, abstractmethod
+import os
+import groq
+from dotenv import load_dotenv
+import json
+import threading
+from groq import Groq
+from google import genai
+from google.genai import types
+from config import (
+    GROQ_MODEL, GROQ_KEY, GEMINI_MODEL, GEMINI_KEY,
+    UTILS_DIR, DEBUG, IN_DEPTH_DEBUG, CEREBRAS_KEY, CEREBRAS_MODEL
+)
+from Exceptions import (
+    NoAPIKeysError, NoModelMentioned, UnknownError
+)
+from cerebras.cloud.sdk import Cerebras
+
+load_dotenv()
+CURR_DIR = UTILS_DIR / "LLMClients.py"
+
+class LLM(ABC):
+    """
+    Each LLM subclass manages its own keys and models.
+    call() returns None when fully exhausted (all keys x all models tried).
+    InitLLM uses this signal to switch to the next client.
+    """
+    @abstractmethod
+    def call(self, user_input, json_mode=False):
+        pass
